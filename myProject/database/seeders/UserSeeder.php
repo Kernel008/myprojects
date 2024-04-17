199e7d290ed88fd7b065807abe2208d11a00d63b
Serdar <srdrtrz@gmail.com>
2024-04-17 03:58:25 +0300

    First Creation

---
+++ myProject/database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+        {
+            $users = [
+                [
+                    'name' => 'Serdar Terzi',
+                    'email' => 'srdrtrz@gmail.com',
+                    'password' => Hash::make('123456')
+                ],
+                [
+                    'name' => 'Serdar Terzi',
+                    'email' => 'srdrtrz2@gmail.com',
+                    'password' => Hash::make('123456')
+                ],
+                [
+                    'name' => 'Serdar Terzi',
+                    'email' => 'srdrtrz3@gmail.com',
+                    'password' => Hash::make('123456')
+                ],            
+            ];
+
+            foreach ($users as $user) {
+                User::insert($user);
+            }
+        }
+
+}
